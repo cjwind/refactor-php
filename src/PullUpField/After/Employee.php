36f0233b0af3b2b3467b073a8129e0400310b686
cjwind <cwentsai@gmail.com>
2021-07-29 21:53:18 +0800

    Add after refactor code

---
+++ src/PullUpField/After/Employee.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Refactor\PullUpField\After;
+
+class Employee
+{
+    protected string $name;
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+}
---
+++ src/PullUpField/After/Salesman.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Refactor\PullUpField\After;
+
+
+class Salesman extends Employee
+{
+
+}
