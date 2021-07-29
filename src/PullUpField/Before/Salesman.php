0d24edb3dadc448199d1b4140f1a61fd4d105c6d
cjwind <cwentsai@gmail.com>
2021-07-29 21:35:56 +0800

    Add getter and setter

--- src/PullUpField/Before/Salesman.php
+++ src/PullUpField/Before/Salesman.php
@@ -7,4 +7,20 @@ namespace Refactor\PullUpField\Before;
 class Salesman extends Employee
 {
     private string $name;
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
 }
